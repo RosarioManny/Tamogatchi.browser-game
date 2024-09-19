3646ae4006dce7337ad28230126cc69555d5a1b5
RosarioManny <rosario.emm47@gmail.com>
2024-09-19 19:14:54 -0400

    Step 1-3

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const boredomStatEl = document.querySelector("#boredom-stat");
+const hungerStatEl = document.querySelector("#hunger-stat")
+const sleepyStatEl= document.querySelector("#sleepiness-stat")
+const gameMessageEl = document.querySelector("#message")
+
+const playButton = document.querySelector("#play")
+const feedButton = document.querySelector("#feed")
+const sleepButton = document.querySelector("#sleep")
+const resetButton = document.querySelector("#restart")
+
+
+const init = () => {
+
+}
+
+init()
+
+const state = {
+    boredom: 0,
+    sleepiness: 0,
+    hunger: 0
+};
+
+// const timer;
+// const gameOver;
+
